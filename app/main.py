63551fbf42e527786f25d758b9ddc5721d3fc586
gmorettirib <guimorettir@gmail.com>
2026-09-24 07:56:03 -0300

    feat: permite acesso do frontend

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy import select
 from sqlalchemy.orm import Session
@@ -8,6 +9,9 @@ from app.schemas import LivroCriacao, LivroResposta
 
 
 BaseBanco.metadata.create_all(bind=mecanismo_banco)
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(
     title="API de Livros",
@@ -15,6 +19,16 @@ app = FastAPI(
     description="API didática para gerenciamento de livros.",
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+    ],
+    allow_credentials=False,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Content-Type"],
+)
 
 @app.post("/livros", response_model=LivroResposta, status_code=201, tags=["Livros"])
 def criar_livro(dados_livro: LivroCriacao, sessao_banco: Session = Depends(obter_sessao_banco)):
